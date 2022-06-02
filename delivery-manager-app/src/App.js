e8c8bd09d29f2c3491f28d42890eafafdf046494
tgoetter <Gittobi471dafbmkot>
2022-06-02 15:39:36 -0300

    Handle the login event

--- delivery-manager-app/src/App.js
+++ delivery-manager-app/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,8 +7,15 @@ import {
 } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
+import Login from './components/Login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className='wrapper'>
       <h1>Application</h1>
